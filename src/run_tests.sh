c1ec42d9f94cbf1d219435ada8f2a51961be6992
Your Name <you@example.com>
2024-06-14 22:17:52 +0200

    Add run_tests.sh to src folder

---
+++ src/run_tests.sh
@@ -0,0 +1,17 @@
+curl -X POST http://localhost:8000 \
+    -H "Content-Type: application/json" \
+    -d '{
+        "input": {
+            "model": "medium",
+            "audio": "https://github.com/runpod-workers/sample-inputs/raw/main/audio/gettysburg.wav"
+        }
+    }'
+curl -X POST http://localhost:8000 \
+    -H "Content-Type: application/json" \
+    -d '{
+        "input": {
+            "model": "medium",
+            "bucket":"transcription",
+            "audio": "test_1.m4a"
+        }
+    }'    
